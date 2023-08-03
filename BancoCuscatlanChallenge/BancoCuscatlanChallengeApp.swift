ad19396123ed4a77d256654b9dcb1153303818be
mcruz <ing.mcruzhernandez@gmail.com>
2023-08-03 12:39:04 -0600

    create project

---
+++ BancoCuscatlanChallenge/BancoCuscatlanChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BancoCuscatlanChallengeApp.swift
+//  BancoCuscatlanChallenge
+//
+//  Created by Alejandro Cruz on 8/3/23.
+//
+
+import SwiftUI
+
+@main
+struct BancoCuscatlanChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
