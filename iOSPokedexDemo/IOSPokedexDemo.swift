66d2a6478c2ab6188340de793ce7c1658fcd0adf
miguel.cruz <acruzh92@gmail.com>
2026-05-28 11:46:01 -0600

    Rename Project

--- iOSPokedexDemo/IOSPokedexDemo.swift
+++ iOSPokedexDemo/IOSPokedexDemo.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct BancoCuscatlanChallengeApp: App {
+struct IOSPokedexDemo: App {
     @StateObject private var modelData = ModelData()
     
     var body: some Scene {
